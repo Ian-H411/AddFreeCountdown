7123f2364b7408f60c189c0814c9a35093bee049
Ian_H411 <ian.h411@gmail.com>
2020-01-07 15:07:15 -0700

    adds AddCountdown VC file

---
+++ AddFreeCountdown/AddFreeCountdown/Views/AddCountdownTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddCountdownTableViewController.swift
+//  AddFreeCountdown
+//
+//  Created by Ian Hall on 1/7/20.
+//  Copyright © 2020 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class AddCountdownTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+   //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   
+    }
+
+}
