319a7d10c7ff7698bb8480d72ead63b144fbbd38
Ian_H411 <ian.h411@gmail.com>
2020-01-09 14:43:20 -0600

    adds camera and image access to the app

--- AddFreeCountdown/AddFreeCountdown/Views/AddCountdownTableViewController.swift
+++ AddFreeCountdown/AddFreeCountdown/Views/AddCountdownTableViewController.swift
@@ -27,12 +27,12 @@ class AddCountdownTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         titleTextField.delegate = self
-        jk
+        
     }
     //MARK: - Actions
     
-    @IBAction func addButtonTapped(_ sender: Any) {
-        
+    @IBAction func addPhotoButtonTapped(_ sender: Any) {
+        presentActionSheet()
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
@@ -45,12 +45,46 @@ class AddCountdownTableViewController: UITableViewController {
         
     }
     
+    func camera(){
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true , completion: nil)
+        }
+    }
+    
+    func photoLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let mypickerController = UIImagePickerController()
+            mypickerController.delegate = self
+            mypickerController.sourceType = .photoLibrary
+            self.present(mypickerController, animated: true, completion: nil)
+        }
+    }
+    
+    func presentActionSheet(){
+        let actionSheet = UIAlertController(title: "Import Receipt Photo", message: nil, preferredStyle: .actionSheet)
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let cameraButton = UIAlertAction(title: "Import With Camera", style: .default) { (_) in
+                self.camera()
+            }
+            actionSheet.addAction(cameraButton)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let photoLibrary = UIAlertAction(title: "Import From Photo Library", style: .default) { (_) in
+                self.photoLibrary()
+            }
+            actionSheet.addAction(photoLibrary)
+        }
+        let cancelButton = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        actionSheet.addAction(cancelButton)
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
     
     //MARK: - Navigation
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-    }
     
 }
 extension AddCountdownTableViewController: UITextFieldDelegate {
@@ -67,5 +101,19 @@ extension AddCountdownTableViewController: UITextFieldDelegate {
             return false
         }
        
+    }
+}
+extension AddCountdownTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            countdownImage.image = image
+        } else {
+            print("Something went wrong")
+        }
+        self.dismiss(animated: true, completion: nil)
     }
 }
