2d1dcd00de1520da2ab2baa9454b24f812e2f677
Ian_H411 <ian.h411@gmail.com>
2020-01-12 12:46:52 -0600

    adds a countdown timer

--- AddFreeCountdown/AddFreeCountdown/Views/CountdownDetailViewController.swift
+++ AddFreeCountdown/AddFreeCountdown/Views/CountdownDetailViewController.swift
@@ -24,11 +24,46 @@ class CountdownDetailViewController: UIViewController {
     
     var countdownLandingPad: Countdown?
     
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initializeUI()
     }
     
-
+    override func viewWillDisappear(_ animated: Bool) {
+        guard let timer = timer else {return}
+        timer.invalidate()
+    }
+    //MARK: - Actions
+    
+    
+    
+    //MARK: - Helper Functions
+    
+    func initializeUI() {
+        guard let countdown = countdownLandingPad else {return}
+        guard let date = countdown.date else {return}
+        titleLabel.text = countdown.name
+        dateLabel.text = DateHelper.shared.dateToString(date: date)
+        countdownImageLabel.image = countdown.photo
+        refreshTimer(date: date)
+        
+    }
+    
+    func refreshTimer(date: Date) {
+        let timerInitialized = Timer(timeInterval: 1.0, repeats: true) { (_) in
+            let seconds = DateHelper.shared.returnCountdown(date: date)
+            let minuetes = seconds / 60
+            let hours = minuetes / 60
+            let days = hours / 24
+            let years = days / 365
+            let countdownAsString = "\(years) years, \(days) days, \(hours) hours, \(minuetes) mins, \(seconds) seconds."
+            self.countdownLabel.text = countdownAsString
+        }
+        timerInitialized.fire()
+        timer = timerInitialized
+    }
+    
 
 }
