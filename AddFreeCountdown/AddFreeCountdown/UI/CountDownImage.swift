1e9239036fbc60ef0e207e63a765e4d334d1d8a6
Ian_H411 <ian.h411@gmail.com>
2020-01-15 20:26:33 -0600

    adds failed attempt at glowing to the image class

--- AddFreeCountdown/AddFreeCountdown/UI/CountDownImage.swift
+++ AddFreeCountdown/AddFreeCountdown/UI/CountDownImage.swift
@@ -7,23 +7,44 @@
 //
 import UIKit
 
-class CountdownImage: UIImage {
+class CountdownImage: UIImageView {
+    
+    var timer: Timer?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         adjustBorderToCircle()
         changeBorderColor()
+        glow()
     }
     
+    
+    
     func adjustBorderToCircle() {
-        
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = self.layer.bounds.width / 2
+        self.layer.borderWidth = 2
     }
     
     fileprivate func changeBorderColor() {
-        
+        self.layer.borderColor = UIColor.yellow.cgColor
     }
     
-    func addGlow() {
+    
+    
+    func glow(){
+        var shadowRadius: CGFloat = 10
+        self.layer.shadowColor = UIColor.yellow.cgColor
+        self.layer.shadowRadius = shadowRadius
+        let timerSet = Timer(fire: Date(), interval: 1.0, repeats: true) { (_) in
+            if shadowRadius < 30 {
+                shadowRadius += 15
+            } else if shadowRadius >= 30 {
+                shadowRadius -= 15
+            }
+        }
+        timerSet.fire()
+        timer = timerSet
         
     }
     
